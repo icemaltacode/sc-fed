11b4824ae413d41c6bad5b2dc12de6f9eff6c85a
Keith Vassallo <keith@icemalta.com>
2025-07-10 13:59:58 +0100

    Migrate to Vite. Module 2, Unit 2.

--- Module2/Unit2/my-react-app/src/main.jsx
+++ Module2/Unit2/my-react-app/src/main.jsx
@@ -1,56 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
+const root = createRoot(document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const boldReact = React.createElement("em", null, "React");
+// const greeting = React.createElement("h1", null, "Hello, ", boldReact, "!");
+// const link = React.createElement("a", { href: "//reactjs.org" }, "Learn more");
+// const rootNode = React.createElement(React.Fragment, null, greeting, link);
+// root.render(rootNode);
 
-const boldReact = React.createElement("em", null, "React");
-const greeting = React.createElement("h1", null, "Hello, ", boldReact, "!");
-const link = React.createElement("a", { href: "//reactjs.org" }, "Learn more");
-const rootNode = React.createElement(React.Fragment, null, greeting, link);
-
-class Link extends React.Component {
-    render() {
-        return React.createElement(
-            "p",
-            null,
-            React.createElement(
-                "a",
-                { href: "//reactjs.org"},
-                "Learn more about React"
-            )
-        );
-    }
-}
+// class Link extends React.Component {
+//     render() {
+//         return React.createElement(
+//             "p",
+//             null,
+//             React.createElement(
+//                 "a",
+//                 { href: "//reactjs.org"},
+//                 "Learn more about React"
+//             )
+//         );
+//     }
+// }
 
 // const link1 = React.createElement(Link);
 // const link2 = React.createElement(Link);
 // const link3 = React.createElement(Link);
 // const linkGroup = React.createElement(React.Fragment, null, link1, link2, link3);
+// root.render(linkGroup);
 
-class CustomLink extends React.Component {
-    render() {
-        const link = React.createElement(
-            "a",
-            { href: this.props.url },
-            `Learn more about ${this.props.framework}`
-        );
-        if (this.props.framework === "React") {
-            return React.createElement(
-                "div",
-                { style: {"text-transform": "uppercase"}},
-                link
-            );
-        } else {
-            return React.createElement(
-                "p",
-                null,
-                link  
-            );
-        }
-    }
-}
-
+// class CustomLink extends React.Component {
+//     render() {
+//         return React.createElement(
+//             "p",
+//             null,
+//             React.createElement(
+//                 "a",
+//                 { href: this.props.url },
+//                 `Learn more about ${this.props.framework}`
+//             )
+//         );
+//     }
+// }
 // const customLink1 = React.createElement(CustomLink, {
 //     framework: "React",
 //     url: "//reactjs.org"
@@ -63,6 +54,7 @@ class CustomLink extends React.Component {
 
 
 // const linkGroup = React.createElement(React.Fragment, null, customLink1, customLink2);
+// root.render(linkGroup);
 
 class LinkWithChildren extends React.Component {
     render() {
@@ -99,4 +91,3 @@ root.render(linkGroup);
 //     "Learn more",
 //   ),
 // );
-
